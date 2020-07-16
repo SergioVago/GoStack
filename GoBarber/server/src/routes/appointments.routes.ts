76eb5c41a5d0d4cb7bd7698b746534613a55e8ae
Sergio Vago <sergio.vago@hotmail.com>
2020-07-16 09:39:52 -0300

    Fim do módulo

--- GoBarber/server/src/routes/appointments.routes.ts
+++ GoBarber/server/src/routes/appointments.routes.ts
@@ -12,7 +12,6 @@ const appointmentsRoutes = Router();
 appointmentsRoutes.use(ensureAuthenticated);
 
 appointmentsRoutes.get('/', async (request, response) => {
-  console.log('request.user :>> ', request.user);
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
   const appointments = await appointmentsRepository.find();
 
@@ -20,22 +19,18 @@ appointmentsRoutes.get('/', async (request, response) => {
 });
 
 appointmentsRoutes.post('/', async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
+  const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
+  const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService();
 
-    const appointment = await createAppointment.execute({
-      provider_id,
-      date: parsedDate,
-    });
+  const appointment = await createAppointment.execute({
+    provider_id,
+    date: parsedDate,
+  });
 
-    return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(appointment);
 });
 
 export default appointmentsRoutes;
